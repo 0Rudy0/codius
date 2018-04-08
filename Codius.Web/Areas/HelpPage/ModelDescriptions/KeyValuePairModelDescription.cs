08433e446b8c0f8bebbc970059a4e16b1e54f1a6
0Rudy0 <rudman0@gmail.com>
2018-04-08 17:16:09 +0200

    Init commit

---
+++ Codius.Web/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Codius.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
